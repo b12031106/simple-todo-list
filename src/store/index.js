ed0afc58e0d9cc52eb2b89e2ea5124024914a110
Justin Hsu <b12031106@gmail.com>
2024-07-29 09:54:17 +0800

    feat: implement data persistence by localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,38 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./slices/tasksSlices";
 
+const LOCAL_STORAGE_KEY = "reduxState";
+
+const loadState = () => {
+    try {
+        const serializedState = localStorage.getItem(LOCAL_STORAGE_KEY);
+        if (serializedState === null) {
+            return undefined;
+        }
+        return JSON.parse(serializedState);
+    } catch (err) {
+        return undefined;
+    }
+};
+
+const saveState = (state) => {
+    try {
+        const serializedState = JSON.stringify(state);
+        localStorage.setItem(LOCAL_STORAGE_KEY, serializedState);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
 const store = configureStore({
     reducer: {
         tasks: tasksReducer,
     },
+    preloadedState: loadState(),
+});
+
+store.subscribe(() => {
+    saveState(store.getState());
 });
 
 export default store;
